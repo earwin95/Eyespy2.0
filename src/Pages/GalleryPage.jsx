3036c11a3dff5714137c0d17de52424340e6575f
earwin <earwin95@gmail.com>
2025-04-28 17:53:18 +0200

    modification du background de la gallerie

--- src/Pages/GalleryPage.jsx
+++ src/Pages/GalleryPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Gallery from '../Components/Gallery';
 
 const GalleryPage = ({ darkMode }) => (
-  <main className={`p-4 ${darkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
+  <main className={`p-4 ${darkMode ? 'bg-gray-900 text-white min-h-screen' : 'bg-white text-black min-h-screen'}`}>
     <h1 className="text-4xl md:text-5xl font-bold text-blue-800 text-center mb-8 tracking-tight animate-fade-in">
     Galerie des captures
 
